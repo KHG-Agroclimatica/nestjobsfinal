025adabe80a0b7d17580464a79aa445c212d55e8
Kevin <khg@agroclimatica.com>
2021-08-18 11:26:15 -0600

    finish testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ReportModule } from './api/report/report.module';
 import configEnv from './config/env.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DBOptions } from './config/db.config';
-import { TaskService } from './jobs/task.service';
-import { TaskModule } from './jobs/task.module';
+import { JobModule } from './modules/job/job.module';
 
 @Module({
   imports: [
@@ -16,11 +14,10 @@ import { TaskModule } from './jobs/task.module';
         load: [configEnv]
       },
     ),
-    ReportModule,
     TypeOrmModule.forRootAsync({
       useClass: DBOptions,
     }),
-    TaskModule,
+    JobModule,
   ]
 })
 export class AppModule {}
